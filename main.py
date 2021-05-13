a979123b17e140405f001b7508c0d615eb204a80
Abhiyan Paudel <abhiyan1997@gmail.com>
2021-05-13 16:33:14 +0545

    Version 1.0

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+random_no= random.randint(1,100)
+guesses= 1
+print("Guess the number:")
+while(guesses<=9):
+
+    input1= int(input())
+
+    if random_no<input1:
+        print("Please guess smaller no.")
+
+
+    elif random_no>input1:
+        print("Please guess greater no.")
+
+
+    else:
+        print(f"Congrats You Have Guessed the right no in {guesses} guesses")
+        break
+
+    print(9-guesses,"no. of guesses left")
+    guesses = guesses + 1
+
+if guesses>9:
+    print("Game Over!!!")
